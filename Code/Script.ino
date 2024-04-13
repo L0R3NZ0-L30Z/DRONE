3783ed4d2594c0f1c19b0e65bfe774c35c224606
Tomás Osorio <tomyeosorio@gmail.com>
2024-04-13 16:38:09 -0300

    Gyro update__actualizaciln giurto

--- Code/Script.ino
+++ Code/Script.ino
@@ -29,6 +29,9 @@ Advertencias:
   IPAddress secondaryDNS(8, 8, 4, 4); // optional
 */
 #include <WiFi.h>
+#include "I2Cdev.h"
+#include "MPU6050.h"
+#include "Wire.h"
 WiFiServer server(80);
 
 const char* ssid = "SSID";                  //REMPLAZAR POR SSID
@@ -86,17 +89,7 @@ void clasify(){
       msj.remove(0, 6);
       //ProcessT(msj.toFloat());
   }
-}/*
-void ProcessT(float var){
-  for(int i=9; i>=0; i++){
-    DatT[i]=DatT[i-1];
-  }
-  DatT[0]= var;
-  for(int o=0; o<=10; o++){
-    DatosApp[2] = DatT[o];}
-  DatosApp[2] =  DatosApp[2] / 10;
-  }*/
-  
+}
 void MotorStart(){
   pinMode(M1,OUTPUT);
   pinMode(M2,OUTPUT);
@@ -209,13 +202,78 @@ void PIDconvert(){
   PW[2] = (PW[2] + (DatosApp[3] / PRDiv)) / 2; 
   PW[3] = (PW[3] + (DatosApp[3] / PRDiv)) / 2; 
 }
+void incialicia_Gyro(){
+  
 
+    // join I2C bus (I2Cdev library doesn't do this automatically)
+    #if I2CDEV_IMPLEMENTATION == I2CDEV_ARDUINO_WIRE
+        Wire.begin();
+    #elif I2CDEV_IMPLEMENTATION == I2CDEV_BUILTIN_FASTWIRE
+        Fastwire::setup(400, true);
+    #endif
+
+    // initialize serial communication
+    // (38400 chosen because it works as well at 8MHz as it does at 16MHz, but
+    // it's really up to you depending on your project)
+    Serial.begin(38400);
+
+    // initialize device
+    /Serial.println("Initializing I2C devices...");
+    accelgyro.initialize();
+
+    // verify connection
+    //Serial.println("Testing device connections...");
+    Serial.println(accelgyro.testConnection() ? "MPU6050 connection successful" : "MPU6050 connection failed");
+
+    // use the code below to change accel/gyro offset values
+    
+    accelgyro.setXGyroOffset(220);
+    accelgyro.setYGyroOffset(76);
+    accelgyro.setZGyroOffset(-85);
+    //Serial.println("Updating internal sensor offsets...");
+    // -76 -2359 1688 0 0 0
+    //Serial.print(accelgyro.getXAccelOffset()); Serial.print("\t"); // -76
+    //Serial.print(accelgyro.getYAccelOffset()); Serial.print("\t"); // -2359
+    //Serial.print(accelgyro.getZAccelOffset()); Serial.print("\t"); // 1688
+    //Serial.print(accelgyro.getXGyroOffset()); Serial.print("\t"); // 0
+    //Serial.print(accelgyro.getYGyroOffset()); Serial.print("\t"); // 0
+    //Serial.print(accelgyro.getZGyroOffset()); Serial.print("\t"); // 0
+    //Serial.print("\n");
+    
+    
+
+    // configure Arduino LED pin for output
+    pinMode(LED_PIN, OUTPUT);
+}
+void lectura(){
+     // read raw accel/gyro measurements from device
+    accelgyro.getMotion6(&ax, &ay, &az, &gx, &gy, &gz);
+
+    // these methods (and a few others) are also available
+    //accelgyro.getAcceleration(&ax, &ay, &az);
+    //accelgyro.getRotation(&gx, &gy, &gz);
+
+    #ifdef OUTPUT_READABLE_ACCELGYRO
+        // display tab-separated accel/gyro x/y/z values
+        //Serial.print("a/g:\t");
+        //Serial.print(ax); Serial.print("\t");
+        //Serial.print(ay); Serial.print("\t");
+        //Serial.print(az); Serial.print("\t");
+        Serial.print(gx); Serial.print("\t");
+        Serial.print(gy); Serial.print("\t");
+        //Serial.println(gz);
+    #endif
+   // blink LED to indicate activity
+    //blinkState = !blinkState;
+    //digitalWrite(LED_PIN, blinkState);
+    }
 
 void setup() {
   Serial.begin(115200);
   WifiStart();                              //INICIO DE RECEPCION DE DATOS
+  incialicia_Gyro();
   //MotorStart();
-  //pinMode(X, INPUT);                      //PIN A DEFINIR PARA CONTROLAR LA CARGA DE LA BATERIA}
+  //pinMode(X, INPUT);                     //PIN A DEFINIR PARA CONTROLAR LA CARGA DE LA BATERIA
   
 }
 
