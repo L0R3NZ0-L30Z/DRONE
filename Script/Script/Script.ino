99b44b1ef7d52b5b1ae58d341a894a0fdd03deee
MESA 01 <you@example.com>
2024-08-09 10:37:47 -0300

    Wifi updt of pid

--- Script/Script/Script.ino
+++ Script/Script/Script.ino
@@ -36,13 +36,11 @@ Advertencias:
 #include <math.h>
 #include <SPI.h>
 #include <Arduino.h>
-#include <Preferences.h>
-#include <esp_system.h>
+
 
 
 WiFiServer server(80);
 Adafruit_MPU6050 mpu;
-Preferences preferences;
 QMC5883LCompass compass;
 
 
@@ -169,72 +167,10 @@ void MPU6050Start() {
   //delay(100);
 }
 
-void readSerialData() {
-  if (Serial.available() > 0) {
-    while (Serial.available()) {
-      char c = Serial.read();
-      incomingData += c;
-      delay(2);
-    }
-    Serial.println("Received data:");
-    Serial.println(incomingData);
-    filterAndStore();
-    incomingData = "";
-  }
-}
-void filterAndStore() {
-  int values[9] = { 0 };
 
-  int index = 0;
-  int startIndex = 0;
-  for (int i = 0; i < incomingData.length(); i++) {
-    if (incomingData[i] == '/') {
-      String valueStr = incomingData.substring(startIndex, i);
-      values[index++] = valueStr.toInt();
-      startIndex = i + 1;
-    }
-  }
-  if (startIndex < incomingData.length()) {
-    String valueStr = incomingData.substring(startIndex);
-    values[index] = valueStr.toInt();
-  }
-  storeValue("KpRoll", values[0]);
-  storeValue("KiRoll", values[1]);
-  storeValue("KdRoll", values[2]);
-  storeValue("KpPitch", values[3]);
-  storeValue("KiPitch", values[4]);
-  storeValue("KdPitch", values[5]);
-  storeValue("KpYaw", values[6]);
-  storeValue("KiYaw", values[7]);
-  storeValue("KdYaw", values[8]);
-  printandset();
-}
 
-void printandset() {
-  KpRoll = printvalue("KpRoll");
-  KiRoll = printvalue("KiRoll");
-  KdRoll = printvalue("KdRoll");
-  KpPitch = printvalue("KpPitch");
-  KiPitch = printvalue("KiPitch");
-  KdPitch = printvalue("KdPitch");
-  KpYaw = printvalue("KpYaw");
-  KiYaw = printvalue("KiYaw");
-  KdYaw = printvalue("KdYaw");
-}
-float printvalue(const char* key) {
-  preferences.begin("my-app", false);
-  float value = preferences.getInt(key, 0);
-  Serial.print(key);
-  Serial.print(": ");
-  Serial.println(value);
-  preferences.end();
-  return value;
-}
-void storeValue(const char* key, int value) {
-  preferences.begin("my-app", false);
-  preferences.putInt(key, value);
-  preferences.end();
-}
+
+
 void WifiConection() {
   WiFiClient client = server.available();
   if (client.available()) {
@@ -243,18 +179,113 @@ void WifiConection() {
       if (c == '\n') { break; }
       msj += c;
     }
-    //Serial.println(msj);
+    Serial.println(msj);
     clasify();
     msj = "";
   }
 }
 void clasify() {
+  if(msj[5] == 'R') {
+    asignData();
+    }
+  else if(msj[5] == 's') {
+    asignPID();
+    }
+}
+
+void asignPID(){
+  String var = "";
+  int i;
+  int cont = 9;
+  for (i = 9; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KpRoll = var.toFloat();
+  var = "";
+  for (i = cont+1; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KiRoll = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KdRoll = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KpPitch = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KiPitch = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KdPitch = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KpYaw = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '&'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KiYaw = var.toFloat();
+  var = "";
+  for (i = cont; msj[i] != '\0'; i++) {
+    var += msj[i];
+    cont++;
+  }
+  KdYaw = var.toFloat();
+
+
+  Serial.print("KpRoll = ");
+  Serial.print(KpRoll);
+  Serial.print(",");
+  Serial.print("KiRoll = ");
+  Serial.print(KiRoll);
+  Serial.print(",");
+  Serial.print("KdRoll = ");
+  Serial.print(KdRoll);
+  Serial.print(",");
+  Serial.print("kpPitch = ");
+  Serial.print(KpPitch);
+  Serial.print(",");
+  Serial.print("KiPitch = ");
+  Serial.print(KiPitch);
+  Serial.print(",");
+  Serial.print("KdPitch = ");
+  Serial.print(KpRoll);
+  Serial.print(",");
+  Serial.print("KpYaw = ");
+  Serial.print(KpYaw);
+  Serial.print(",");
+  Serial.print("KiYaw = ");
+  Serial.print(KiYaw);
+  Serial.print(",");
+  Serial.print("KdYaw = ");
+  Serial.print(KdYaw);
+  Serial.print(",");
+}
+void asignData(){
   /*GET /Res?ID=8896&Slider=0&XGyro=0.005&YGyro=0.015&0x=-2.6025&0y=3.82375 HTTP/1.1*/
   int temp;
   String var = "";
   for (int i = 12; i <= 16; i++) var += msj.charAt(i);
   temp = var.toInt();
-
   if (TimingVar > 9800 && temp < 1150) {
     assign();
     TimingVar = temp;
@@ -428,33 +459,25 @@ void setup() {
   BrushlessM3.setPeriodHertz(50);
   BrushlessM4.setPeriodHertz(50);
   MotorStart();
-  MPU6050Start();
+  /*MPU6050Start();
   compass.init();
-  DatosMagnetometro[2] = Magnetometro();
-  Serial.println("Tiempo para actualizar valores del PID");
-  Serial.println("Formato: KpRoll/KiRoll/KdRoll/KpPitch/KiPitch/KdPitch/KpYaw/KiYaw/KdYaw");
-  for (int i = 0; i < 2000; i++) {
-    readSerialData();
-    delay(1);
-  }
-  Serial.println("Finalizo para actualizar valores del PID");
-  printandset();
+  DatosMagnetometro[2] = Magnetometro();*/
 }
 void loop() {  //NO PONER DELAYS!!!!!!!
   int bri = 0;
   WifiConection();  //RECEPCION DE DATOS
-  Acelerometro();   //INPUT DEL GIROSCOPIO
+  /* Acelerometro();   //INPUT DEL GIROSCOPIO
   Magnetometro();   //INPUT DEL MAGNETOMETRO PIDRoll();                              //PID ROLL
   PIDPitch();       //PID PITCH
   PIDYaw();         //PID YAW PID
   PIDconvert();                             //SUMA DE LOS OUTPUT DE LOS PID
   MotorDriver();
   bri = DatosApp[0] * (17 / 12);
-  analogWrite(2, bri);
+  analogWrite(2, bri); */
   //delay(20);                                //UNICO DELAY PARA DEJA PROCESAR
 
 
-  Serial.print("Signal Streght: ");
+/*   Serial.print("Signal Streght: ");
   Serial.print(WiFi.RSSI());
   Serial.print(","); 
   Serial.print("Slider: ");
@@ -488,4 +511,4 @@ void loop() {  //NO PONER DELAYS!!!!!!!
   Serial.print(DatosAcelerometro[1]);
   Serial.print(",");
   Serial.println("uT");
-}
+ */}
